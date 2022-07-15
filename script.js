ed6da87185175cb957f5cf35ffe28c2c1e4ab342
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-07-15 18:44:19 +0100

    updating the mobile navigation menu

---
+++ script.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector("#ham_burger");
+const cross = document.querySelector(".mobile_menu p");
+const mobileMenu = document.querySelector(".mobile_menu");
+
+
+hamburger.addEventListener("click",()=>{
+    // mobileMenu.style.display = "block";
+    mobileMenu.style.width = "70%"
+})
+
+cross.addEventListener("click",()=>{
+    mobileMenu.style.width = "0%"
+    // mobileMenu.style.display = "none";
+})
