621fdf2673a3a3acab032c7b3722e2e91078e2cc
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-08-01 22:50:07 +0100

    added an animation to the page pre loader

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const hamburger = document.querySelector("#ham_burger");
+const loader = document.querySelector('#loader');
+const myDiv = document.querySelector('#myDiv')
 const cross = document.querySelector(".mobile_menu p");
 const mobileMenu = document.querySelector(".mobile_menu");
 
 
 hamburger.addEventListener("click",()=>{
     mobileMenu.style.display = "block";
-    mobileMenu.style.width = "70%"
+    mobileMenu.style.width = "70%";
 })
 
 cross.addEventListener("click",()=>{
-    mobileMenu.style.width = "0%"
+    mobileMenu.style.width = "0%";
     mobileMenu.style.display = "none";
 })
+
+
+
+
+
+function showPage(){
+    setTimeout(() => {
+         loader.style.display ='none';
+         myDiv.style.display ='block';
+    }, 5000);
+
+}
